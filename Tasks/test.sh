c661ba887b3f169fd5d13de009c9d68c40f6a997
Pamela Tabak <pamelatabak@poli.ufrj.br>
2017-04-21 20:18:22 -0300

    shell script to execute processes is finished

--- Tasks/test.sh
+++ Tasks/test.sh
@@ -5,43 +5,51 @@ number_of_nodes=1000
 number_of_iterations=50
 echo $number_of_nodes
 
-#targeted_nodes_list="2 4 8 16 32 64 100 128 200 256 300 400 500 512 600 700 800 900 1000"
-#p_list="0.01 0.05 0.08 0.1 0.2 0.3 0.4 0.5"
+targeted_nodes_list="2 4 8 16 32 64 100 128 200 256 300 400 500 512 600 700 800 900 1000"
+p_list="0.01 0.05 0.08 0.1 0.2 0.3 0.4 0.5"
 
-for ((i=0; i < $number_of_iterations; ++i ))
+for target in $targeted_nodes_list;
 do
-	graph=`python generate_gnp.py $number_of_nodes 0.1`
-	
-	targeted_nodes=`python generate_targeted_nodes.py 2`
-	
-	current_dir=$(pwd)
-	output_folder="$current_dir/Files/"
-	
-	#create attackers
-	cd ..
-	cd CreateAttackers/cmake-build-debug
-	./CreateAttackers "$graph" "$targeted_nodes" "$output_folder" edgesPerLine
-	
-	#create new file appending the subgraph generated to the graph G
-	cd ..
-	cd ..
-	cd Tasks
-	current_dir=$(pwd)
-	full_graph=`python append_files.py "$graph" "$current_dir/Files/subgraph.txt"`
-	
-	#anonimize
-	cd ..
-	cd RandomPermutation/cmake-build-debug
-	./RandomPermutation "$full_graph" "$output_folder" edgesPerLine
-	
-	cd ..
-	cd ..
-	
-	#recover graph
-	cd RecoverGraph/cmake-build-debug
-	./RecoverGraph "$full_graph" "$current_dir/Files/degree.txt" "$output_folder" edgesPerLine "$current_dir/Files/subgraph.txt"
-
-	cd ..
-	cd ..
-	cd Tasks
+	for p in $p_list;
+	do
+		echo "$target"
+		echo "$p"
+		for ((i=0; i < $number_of_iterations; ++i ))
+		do
+			graph=`python generate_gnp.py $number_of_nodes $p`
+			
+			targeted_nodes=`python generate_targeted_nodes.py $target`
+			
+			current_dir=$(pwd)
+			output_folder="$current_dir/Files/"
+			
+			#create attackers
+			cd ..
+			cd CreateAttackers/cmake-build-debug
+			./CreateAttackers "$graph" "$targeted_nodes" "$output_folder" edgesPerLine
+			
+			#create new file appending the subgraph generated to the graph G
+			cd ..
+			cd ..
+			cd Tasks
+			current_dir=$(pwd)
+			full_graph=`python append_files.py "$graph" "$current_dir/Files/subgraph.txt"`
+			
+			#anonimize
+			cd ..
+			cd RandomPermutation/cmake-build-debug
+			./RandomPermutation "$full_graph" "$output_folder" edgesPerLine
+			
+			cd ..
+			cd ..
+			
+			#recover graph
+			cd RecoverGraph/cmake-build-debug
+			./RecoverGraph "$full_graph" "$current_dir/Files/degree.txt" "$output_folder" edgesPerLine "$current_dir/Files/subgraph.txt"
+		
+			cd ..
+			cd ..
+			cd Tasks
+		done
+	done
 done
