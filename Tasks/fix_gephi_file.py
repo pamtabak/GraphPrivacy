bacf4022df46dd58200dbb666bc5fe34c0e78c5c
Pamela Tabak <pamelatabak@poli.ufrj.br>
2017-05-30 22:01:20 -0300

    updating tcc and log files

---
+++ Tasks/fix_gephi_file.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+filename  = 'Files/gnp_attack.txt'
+with open(filename, 'w') as outfile:
+	with open('Files/full_graph.txt') as infile:
+		for line in infile:
+			outfile.write(line.replace("\n","") + " Undirected\n")
+
+outfile.close();
